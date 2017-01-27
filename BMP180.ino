da4f6142fc2dfb8d8dee8bebf71fe0aee4ad1dfd
Karel Zanáška <karelzanaska@gmail.com>
2017-01-28 00:06:34 +0100

    Create BMP180.ino

---
+++ BMP180.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp180;
+
+int correction = 32;
+int index = 1;
+
+void setup() {
+  Serial.begin(9600);
+  bmp180.begin();
+}
+
+void loop() {
+  Serial.print("Index: ");
+  Serial.print(index);
+  
+  Serial.print("Teplota [stupen Celsia]: ");
+  Serial.print(bmp180.readTemperature());
+
+  Serial.print("Tlak [Pascal]: ");
+  Serial.print((bmp180.readPressure() + correction*100)/100.00);
+
+  Serial.print("Nadmorska vyska [metr]: ");
+  Serial.print(bmp180.readAltitude(bmp180.readPressure() + correction*100));
+  
+  delay(3000);
+}
